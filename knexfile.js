c663991b6eb0a1c37aa724ebeef8a9d2a1cdaa87
nsolimine <32491766+nsolimine@users.noreply.github.com>
2018-02-07 15:11:00 -0700

    first commit

---
+++ knexfile.js
@@ -0,0 +1,13 @@
+
+module.exports = {
+
+  development: {
+    client: 'pg',
+    connection: 'postgresql:///memory2'
+  },
+
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL
+  }
+};
